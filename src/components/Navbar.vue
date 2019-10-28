34fa31449e50913b8c58e8634104260aa60833b7
shaohongye <373177648@qq.com>
2019-10-28 16:56:38 +0800

    首页

---
+++ src/components/Navbar.vue
@@ -0,0 +1,131 @@
+<template>
+   <Row>
+       <Col :lg="24" :md="24" :sm="0" :xs="0">
+          <Menu mode="horizontal" theme="light" active-name="1">
+            <div class="navbar">
+              <div class="logo">
+                <img class="logo-img" src="https://cdn.demo.fastadmin.net/assets/addons/blog/default/img/logo.jpg">
+              </div>
+              <div class="layout-nav">
+                <MenuItem name="1">
+                  <Icon type="ios-navigate"></Icon>
+                  Item 1
+                </MenuItem>
+                <MenuItem name="2">
+                  <Icon type="ios-keypad"></Icon>
+                  Item 2
+                </MenuItem>
+                <MenuItem name="3">
+                  <Icon type="ios-analytics"></Icon>
+                  Item 3
+                </MenuItem>
+                <MenuItem name="4">
+                  <Icon type="ios-paper"></Icon>
+                  Item 4
+                </MenuItem>
+              </div>
+            </div>
+          </Menu>
+       </Col>
+       <Col :lg="0" :md="0" :sm="24" :xs="24">
+          <div class="navbar-menu">
+               <div class="logo">
+                <img class="logo-img" src="https://cdn.demo.fastadmin.net/assets/addons/blog/default/img/logo.jpg">
+              </div>
+               <div class="layout-nav-sm">
+                  <Icon @click="toggleMobileMenu" type="ios-menu" size="45" style="margin-top:8px" color="#000"/>
+               </div>
+          </div>
+        <Drawer  placement="left" :closable="false" v-model="showMobileMenu">
+           <Menu theme="light" active-name="1">
+          <MenuGroup>
+            <MenuItem name="1">
+                <Icon type="md-document" />
+                文章管理
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="md-chatbubbles" />
+                评论管理
+            </MenuItem>
+             <MenuItem name="2">
+                <Icon type="md-chatbubbles" />
+                评论管理
+            </MenuItem>
+          </MenuGroup>
+        </Menu>
+        </Drawer>
+       </Col>
+     </Row>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'navbar',
+  data()  {
+    return {
+        showMobileMenu : false
+    }
+  },
+  mounted(){
+     window.onreset = () =>{
+       if(window.innerWidth >= 768){
+         this.showMobileMenu = false
+       }
+     }
+  },
+  methods: {
+    toggleMobileMenu(){
+      this.showMobileMenu = !this.showMobileMenu
+    
+    }
+  }
+
+}
+</script>
+<style scoped>
+
+.ivu-menu{
+  height: 80.75px;
+}
+ .navbar {
+  max-width: 1140px;
+  margin: auto;
+}
+.logo{
+  background: red;
+  width: 210px;
+  height: 72px;
+  float: left;
+  position: relative;
+  top: 5px;
+  left: 20px;
+}
+.logo .logo-img {
+    width: 210px;
+    height: 72px;
+  }
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+.navbar-menu{
+  width: 100%;
+  height: 80.75px;
+  background: #FFFFFF;
+}
+.layout-nav-sm .ivu-icon{
+  font-size: 45px;
+    color: rgb(0, 0, 0);
+    position: relative;
+    left: 49px;
+    top: 14px;
+}
+.ivu-btn-icon-only.ivu-btn-large{
+  width: 49.25px;
+  height: 35.25px;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,110 +1,51 @@
 <template>
-   <Row>
-       <Col :lg="24" :md="24" :sm="0" :xs="0">
-          <Menu mode="horizontal" theme="light" active-name="1">
-            <div class="navbar">
-              <div class="logo">
-                <img class="logo-img" src="https://cdn.demo.fastadmin.net/assets/addons/blog/default/img/logo.jpg">
-              </div>
-              <div class="layout-nav">
-                <MenuItem name="1">
-                  <Icon type="ios-navigate"></Icon>
-                  Item 1
-                </MenuItem>
-                <MenuItem name="2">
-                  <Icon type="ios-keypad"></Icon>
-                  Item 2
-                </MenuItem>
-                <MenuItem name="3">
-                  <Icon type="ios-analytics"></Icon>
-                  Item 3
-                </MenuItem>
-                <MenuItem name="4">
-                  <Icon type="ios-paper"></Icon>
-                  Item 4
-                </MenuItem>
-              </div>
-            </div>
-          </Menu>
-       </Col>
-       <Col :lg="0" :md="0" :sm="24" :xs="24">
-          <div class="navbar-menu">
-               <div class="logo">
-                <img class="logo-img" src="https://cdn.demo.fastadmin.net/assets/addons/blog/default/img/logo.jpg">
-              </div>
-               <div class="layout-nav-sm">
-                  <Icon  type="ios-menu" size="45" style="margin-top:8px" color="#000"/>
-               </div>
-          </div>
-        <Menu theme="light" active-name="1">
-          <MenuGroup>
-            <MenuItem name="1">
-                <Icon type="md-document" />
-                文章管理
-            </MenuItem>
-            <MenuItem name="2">
-                <Icon type="md-chatbubbles" />
-                评论管理
-            </MenuItem>
-             <MenuItem name="2">
-                <Icon type="md-chatbubbles" />
-                评论管理
-            </MenuItem>
-          </MenuGroup>
-        </Menu>
-       </Col>
-     </Row>
- </div>
+
+  <Layout>
+   <Navbar></Navbar>
+   <Content>
+   <Row type="flex" :gutter="10" justify="center">
+        <Col  span="11" :xs="24" :sm="24" :md="11" :lg="11">
+        <div class="layout-left">
+           左边内容
+        </div>
+        </Col>
+
+        <Col  span="4" :xs="15" :sm="15" :md="4" :lg="4" >
+        <div class="layout-right">
+           右边内容
+        </div>
+
+        </Col>
+
+      </Row> 
+   </Content>
+   <Footer style="background:#ffff; margin-top:50px" >
+     Copyright @ 2017~2019 Theme design by jeanstudio
+     </Footer>
+  </Layout>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Navbar from "@/components/Navbar.vue";
 export default {
-  name: 'home',
+  name: "home",
+  data() {
+    return {
 
-}
+    };
+  },
+  components: {
+    Navbar
+  }
+};
 </script>
 <style scoped>
 
-.ivu-menu{
-  height: 80.75px;
-}
- .navbar {
-  max-width: 1140px;
-  margin: auto;
-}
-.logo{
+.layout-left {
   background: red;
-  width: 210px;
-  height: 72px;
-  float: left;
-  position: relative;
-  top: 5px;
-  left: 20px;
 }
-.logo .logo-img {
-    width: 210px;
-    height: 72px;
-  }
-.layout-nav{
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
+.layout-right {
+  background: blue;
 }
-.navbar-menu{
-  width: 100%;
-  background: yellow;
-  height: 80.75px;
-}
-.layout-nav-sm {
-      position: relative;
-      left: 54px;
-      top: 22px;
-}
-.ivu-btn-icon-only.ivu-btn-large{
-  width: 49.25px;
-  height: 35.25px;
-}
-
 </style>
